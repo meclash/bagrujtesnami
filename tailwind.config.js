cafce452876c6fff58a3cda1b20a133a753b2d4c
meclash <56850693+meclash@users.noreply.github.com>
2021-06-16 15:51:20 +0200

    working on the section design, imported new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      
+      colors:{
+        harvest: "#E79407",
+        khaki: "#E1E289",
+        quicksilver: "#A1A0A2",
+        spacecadet: "#2B2D42",
+
+      },
     },
   },
   variants: {
