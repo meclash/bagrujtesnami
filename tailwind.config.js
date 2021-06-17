d0f3971f927f1ceb42f3621e5826ceb2ec90b147
meclash <56850693+meclash@users.noreply.github.com>
2021-06-17 05:01:57 +0200

    2 sections done

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,17 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      backgroundImage: theme => ({
+
+      'grasspattern': "url('/media/grass-3.webp')",
+      'rockpattern': "url('/media/sand.webp')",
+
+
+    }),
+
       colors:{
         harvest: "#E79407",
-        khaki: "#E1E289",
+        brandyellow: "#F3FF4A",
         quicksilver: "#A1A0A2",
         spacecadet: "#2B2D42",
 
